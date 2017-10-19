54dc293ca381aec732ab90efe7e50d5e40b4931d
jm <heartfeats@gmail.com>
2017-10-19 13:50:44 -0700

    add working app

--- RockPaperScissors/Controllers/HomeController.cs
+++ RockPaperScissors/Controllers/HomeController.cs
@@ -5,10 +5,27 @@ using RockPaperScissors.Models;
 
 namespace RockPaperScissors.Controllers {
     public class HomeController : Controller {
+
         [HttpGet ("/")]
         public ActionResult Index () {
             return View ();
         }
-        
+
+        // [HttpGet ("/PlayerOne")]
+        // public ActionResult PlayerOne () {
+        //     return View ();
+        // }
+
+        // [HttpPost ("/PlayerTwo")]
+        // public ActionResult PlayerTwo () {
+        //     return View ();
+        // }
+
+        [HttpPost ("/Result")]
+        public ActionResult Result () {
+            PlayerChoices newPlayerChoices = new PlayerChoices(Request.Form["inputOne"], Request.Form["inputTwo"]);
+            return View (newPlayerChoices);
+        }
+
     }
 }
