85c4bf57d18ed995d3f99cd00bcf30ed953e6a99
jm <heartfeats@gmail.com>
2017-10-19 10:51:34 -0700

    Add all initial files

---
+++ RockPaperScissors.Tests/RockPaperScissorsTests.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RockPaperScissors.Models;
+
+namespace RockPaperScissors.Tests {
+
+    [TestClass]
+    public class PlayerChoices {
+
+        [TestMethod]
+        public void PlayerOneWins () {
+            // Arrange
+
+            // Act
+
+            // Assert
+        }
+    }
+}
---
+++ RockPaperScissors/Models/PlayerChoices.cs
@@ -0,0 +1,7 @@
+using System.Collections.Generic;
+
+namespace RockPaperScissors {
+    public class PlayerChoices {
+
+    }
+}
